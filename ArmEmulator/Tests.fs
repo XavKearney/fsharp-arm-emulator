a74059b4c3663874ca350aa9294433d1da17914f
Xav Kearney <xavkearney@gmail.com>
2018-02-05 14:42:34 +0000

    Removed failing sample test

--- ArmEmulator/Tests.fs
+++ ArmEmulator/Tests.fs
@@ -9,7 +9,4 @@ let tests =
       let subject = true
       Expect.isTrue subject "I compute, therefore I am."
 
-    testCase "should fail" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false."
   ]
