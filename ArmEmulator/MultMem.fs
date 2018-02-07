0a6b99365d7a220bc3cf0e2ca2960967ff8cd9e7
Xav Kearney <xavkearney@gmail.com>
2018-02-07 12:46:17 +0000

    Add files to proj, define initial instr type

--- ArmEmulator/MultMem.fs
+++ ArmEmulator/MultMem.fs
@@ -1,13 +1,32 @@
+/// handles LDM/STM instructions
+/// author: Xav Kearney
 module MultMem
-
+    
     open CommonLex
+    open CommonData
 
     /// sample specification for set of instructions
 
     // change these types as required
 
-    /// instruction (dummy: must change)
-    type Instr =  {MemDummy: Unit}
+    type multIns = LDM | STM
+    type multDirection = FD | FA | ED | EA
+    
+    /// multiple memory access instruction type
+    type multMemInstr = 
+        {
+            // load or store
+            InsType: multIns;
+            // stack direction
+            Direction: multDirection;
+            // target register: source/destination
+            Target: RName;
+            // optional writeback suffix '!'
+            // writes the address of the final mem location back to target reg
+            WriteBack: bool;
+            // list of registers to store/load to/from
+            RegList: RName list;
+        }
 
     /// parse error (dummy, but will do)
     type ErrInstr = string
@@ -22,11 +41,15 @@ module MultMem
         // for LDM: "EA" and "DB" are synonyms
         // for STM: "", "EA" and "IA" are synonyms
         // for STM: "FD" and "DB" are synonyms
-        Suffixes = [""; "EA"; "IA"; "FD"; "DB"]
+        Suffixes = [""; "EA"; "ED"; "IA"; "FA"; "FD"; "DB"]
     }
 
     /// map of all possible opcodes recognised
-    let opCodes = opCodeExpand memSpec
+    let opCodes = opCodeExpand multMemSpec
+
+    let makeLDM suf cond =
+
+
 
     /// main function to parse a line of assembler
     /// ls contains the line input
@@ -35,13 +58,40 @@ module MultMem
     /// otherwise it is Ok Parse or Error (parse error string)
     let parse (ls: LineData) : Result<Parse<Instr>,string> option =
         let parse' (instrC, (root,suffix,pCond)) =
-            // this does the real work of parsing
-            // dummy return for now
-            Ok { PInstr={MemDummy=()}; PLabel = None ; PSize = 4u; PCond = pCond }
+            match instrC, root with
+            | MEM, LDM -> makeLDM suffix pCond
+            | MEM, STM -> makeSTM suffic pCond
+            | _ -> Error ("%s, %s not supported" instrC, root)
         Map.tryFind ls.OpCode opCodes
         |> Option.map parse'
 
+    // Ok { 
+    //     // Normal (non-error) return from result monad
+    //     // This is the instruction determined from opcode, suffix and parsing
+    //     // the operands. Not done in the sample.
+    //     // Note the record type returned must be written by the module author.
+    //     PInstr={DPDummy=()}; 
 
+
+    //     // This is normally the line label as contained in
+    //     // ls together with the label's value which is normally
+    //     // ls.LoadAddr. Some type conversion is needed since the
+    //     // label value is a number and not necessarily a word address
+    //     // it does not have to be div by 4, though it usually is
+    //     PLabel = ls.Label |> Option.map (fun lab -> lab, la) ; 
+
+
+    //     // this is the number of bytes taken by the instruction
+    //     // word loaded into memory. For arm instructions it is always 4 bytes. 
+    //     // For data definition DCD etc it is variable.
+    //     //  For EQU (which does not affect memory) it is 0
+    //     PSize = 4u; 
+
+    //     // the instruction condition is detected in the opcode and opCodeExpand                 
+    //     // has already calculated condition already in the opcode map.
+    //     // this part never changes
+    //     PCond = pCond 
+    //     }
 
     /// Parse Active Pattern used by top-level code
     let (|IMatch|_|)  = parse
