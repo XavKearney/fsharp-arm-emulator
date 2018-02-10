7e8b87ebb94f4e3820c0994f2ea188f97dec1837
Alistair Wallace <apsw@wallace2.com>
2018-02-10 18:59:05 +0000

    First commit of memInstructions.fs nout useful yet

---
+++ ArmEmulator/memInstructions.fs
@@ -0,0 +1,33 @@
+//MemInstructions
+
+let LDR = 
+    //insert psuedo code here
+    //Eg: LDR r8, [r10]     //Loads r8 with the value 
+                            // from the address in r10
+    //Eg:LDR r2, [r5,#960]! //Loads r2 with the value
+                            // from the address 960 bytes
+                            // above the address stored
+                            // in r5 and increments r5 by
+                            // 960           
+    0
+
+let STR = 
+    //insert psuedo code here
+    0
+
+let ADR = 
+    //insert psuedo code here
+    0
+
+let DCD = 
+    //insert psuedo code here
+    0
+
+let EQU = 
+    //insert psuedo code here
+    0
+
+let FILL = 
+    //insert psuedo code here
+    0
+    
