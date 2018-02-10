b8434bc967424d21c7b9174111cb031bd7d45645
Alistair Wallace <apsw@wallace2.com>
2018-02-10 18:57:30 +0000

    first commit just updating dependencies

--- ArmEmulator/Main.fs
+++ ArmEmulator/Main.fs
@@ -1,7 +1,15 @@
 module ArmEmulator
 
 open Expecto
+open Expecto.ExpectoFsCheck
+open System
+open System.Xml.Linq
+open System.Xml.Linq
+// open memInstructions
 
 [<EntryPoint>]
 let main argv =
     Tests.runTestsInAssembly defaultConfig argv
+    printfn "enter any key to finish"
+    Console.ReadKey() |> ignore
+    0
