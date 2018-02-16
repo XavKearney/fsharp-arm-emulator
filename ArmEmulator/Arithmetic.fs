1396e4ec80984e1c2146cdcf1272a8ec540ad384
djb15 <dirk.judd@live.com>
2018-02-16 19:30:17 +0000

    Added flex op2 basic structure

--- ArmEmulator/Arithmetic.fs
+++ ArmEmulator/Arithmetic.fs
@@ -1,10 +1,16 @@
 module Arithmetic
     open CommonData
     open CommonLex
+    open System.Text.RegularExpressions
 
     type ArithInstrType = ADD | ADC | SUB | SBC | RSB | RSC
 
     type OpCode = Target of RName | Value of uint32
+
+    type Operations = LSL | ASR | LSR | ROR | RRX
+
+    type Op2Types = Literal of uint32 | Register of RName | RegisterShift of RName * Operations * int32 | RegisterRegisterShift of RName * Operations * RName
+
 
     type ArithInstr = 
         {
@@ -16,8 +22,8 @@ module Arithmetic
             Target: RName;
             // First operation -> must be a register
             Op1: RName;
-            // Second Operation -> Can be both register or value
-            Op2: OpCode;
+            // Second Operation -> Flexible operand 2
+            Op2: Op2Types;
         }
 
     let ArithSpec = {
@@ -28,20 +34,114 @@ module Arithmetic
 
     let opCodes = opCodeExpand ArithSpec
 
+    /// FlexOp2 code that performs shifts on flexible operand 2 
+    let flexOp2 op2 cpuData = 
+        match op2 with
+        //Literal integer
+        | Literal integer -> integer
+        
+        // Contents of specified register
+        | Register reg -> 
+            match cpuData with 
+            | {Fl = _ ; Regs = regmap } -> Map.find reg regmap
+        
+        | RegisterShift (reg, op, integer) -> 
+            // Register shift with various possible operations
+            match op with
+            // Logical shift left
+            | LSL _ -> 
+                match cpuData with 
+                | {Fl = _ ; Regs = regmap } -> Map.find reg regmap <<< integer
+            
+            // Logical shift right
+            | LSR _ -> 
+                match cpuData with 
+                | {Fl = _ ; Regs = regmap } -> Map.find reg regmap >>> integer
+
+            // Arithmetic Shift Right
+            | ASR _ -> 
+                match cpuData with
+                | {Fl = _ ; Regs = regmap } -> uint32(int32(Map.find reg regmap) >>> integer)
+            
+            // RRX puts 1 in MSB if C is true
+            | RRX _ -> 
+                match cpuData with 
+                | {Fl = flag ; Regs = regmap } -> 
+                    match flag with
+                    | {N = _ ; C = carry ; Z = _ ; V = _} -> 
+                        let carryNum = match carry with | true -> uint32(1) | false -> uint32(0)
+                        (Map.find reg regmap >>> 1) + (carryNum <<< 31)
+                
+            // Rotate right
+            | ROR _ ->
+                match cpuData with
+                | {Fl = _ ; Regs = regmap } -> (Map.find reg regmap <<< (32 - integer)) + (Map.find reg regmap >>> integer)
+
+        
+        | RegisterRegisterShift (reg, op, reg2) ->
+            let integer = match cpuData with 
+                          | {Fl = _ ; Regs = regmap } -> int32(Map.find reg2 regmap &&& uint32(31))
+
+            // Register shift with various possible operations
+            match op with
+            // Logical shift left
+            | LSL _ -> 
+                match cpuData with 
+                | {Fl = _ ; Regs = regmap } -> Map.find reg regmap <<< integer
+            
+            // Logical shift right
+            | LSR _ -> 
+                match cpuData with 
+                | {Fl = _ ; Regs = regmap } -> Map.find reg regmap >>> integer
+
+            // Arithmetic Shift Right
+            | ASR _ -> 
+                match cpuData with
+                | {Fl = _ ; Regs = regmap } -> uint32(int32(Map.find reg regmap) >>> integer)
+            
+            // RRX puts 1 in MSB if C is true
+            | RRX _ -> 
+                match cpuData with 
+                | {Fl = flag ; Regs = regmap } -> 
+                    match flag with
+                    | {N = _ ; C = carry ; Z = _ ; V = _} -> 
+                        let carryNum = match carry with | true -> uint32(1) | false -> uint32(0)
+                        (Map.find reg regmap >>> 1) + (carryNum <<< 31)
+                
+            // Rotate right
+            | ROR _ ->
+                match cpuData with
+                | {Fl = _ ; Regs = regmap } -> (Map.find reg regmap <<< (32 - integer)) + (Map.find reg regmap >>> integer)
+
+
+
 
     let parseOpsLine (line:string) = 
+
+        // Flexible operand 2 string parsing
+        let (|FlexParse|_|) pattern input = 
+            let flexMatch = Regex.Match(input, pattern)
+            match flexMatch.Success with
+            | true -> Some "hello"
+            | false -> None
+
         // Get list of operands/destination register
         let operandsList = line.Split(",")
                            |> Array.map (fun str -> str.Trim())
 
+
         match operandsList.Length with
-        | 3 ->
+        | 3 | 4 ->
             // Destination register string
             let destinationStr = operandsList.[0]
             // Op1 string
             let op1Str = operandsList.[1]
+
             // Op2 string
-            let op2Str = operandsList.[2]
+            let op2Str = 
+                match operandsList.Length with
+                | 3 -> operandsList.[2]
+                | _ -> String.concat "," operandsList.[2..]
 
             // Partial active pattern match to see flex op 2 type - pattern match of '#'
             let (|Prefix|_|) (p:string) (s:string) =
