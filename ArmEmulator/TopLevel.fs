a8178f7c92af8b4f321ff4e2d3d3dee0120e7847
Xav Kearney <xavkearney@gmail.com>
2018-03-06 12:12:06 +0000

    Create TopLevel Module

--- ArmEmulator/TopLevel.fs
+++ ArmEmulator/TopLevel.fs
@@ -1,16 +1,18 @@
 module TopLevel
     open CommonLex
     open CommonData
+    open Arithmetic
+    open BitArithmetic
+    open MemInstructions
+    open MultMem
 
     /// allows different modules to return different instruction types
     type Instr =
         // arithmetic instrucitons
         | IARITH of Arithmetic.ReturnInstr
         // bit arithmetic instructions
-        // TODO: needs renaming
         | IBITARITH of BitArithmetic.InstDecomp
         // memory instructions
-        // TODO: Merge into one ReturnInstr
         | IMEM of MemInstructions.MemInstr
         | IADR of MemInstructions.ADRInstr
         | ILABEL of MemInstructions.labelInstr
@@ -27,8 +29,6 @@ module TopLevel
         | ERRIMULTMEM of MultMem.ErrInstr
         | ERRTOPLEVEL of string
 
-    type CondInstr = Condition * Instr
-
     /// Note that Instr in Mem and DP modules is NOT same as Instr in this module
     /// Instr here is all possible instruction values combined with a D.U.
     /// that tags the Instruction class
@@ -41,6 +41,10 @@ module TopLevel
         | BitArithmetic.IMatch pa -> pConv IBITARITH ERRIBITARITH pa
         | MultMem.IMatch pa -> pConv IMULTMEM ERRIMULTMEM pa
         | _ -> None
+    
+    
+
+    type CondInstr = Condition * Instr
 
     let parseLine (symtab: SymbolTable option) (loadAddr: WAddr) (asmLine:string) =
         /// put parameters into a LineData record
@@ -78,102 +82,11 @@ module TopLevel
                         with Label=Some label} 
                       |> IMatch with
                 | None -> 
-                    Error (ERRTOPLEVEL 
-                        (sprintf"Instruction not implemented: %A" (String.concat " " words)))
+                    Error (ERRTOPLEVEL (sprintf "Unimplemented instruction %s" opc))
                 | Some pa -> pa
-            | _ -> 
-                Error (ERRTOPLEVEL 
-                    (sprintf "Invalid instruction: %A" (String.concat " " words)))
+            | _ -> Error (ERRTOPLEVEL (sprintf "Unimplemented instruction %A" words))
         asmLine
         |> removeComment
         |> splitIntoWords
         |> Array.toList
         |> matchLine
-
-
-    /// initialise DataPath object before executing instructions
-    /// takes optional flags/regs/mem to initialise with
-    /// otherwise, all empty to begin with (false flags, zero regs, no memory allocated)
-    let initDataPath flags regs mem = 
-        let initFlags = 
-            match flags with
-            | Some f -> f
-            | None -> {N=false;Z=false;C=false;V=false;} 
-        let initRegs = 
-            match regs with
-            | Some r when Map.count r = 16 -> Ok r
-            | None ->
-                [0..15]
-                |> List.map (fun i -> inverseRegNums.[i], 0u)
-                |> Map.ofList
-                |> Ok
-            | _ -> Error (ERRTOPLEVEL "Invalid initial registers.")
-        let initMem = 
-            match mem with
-            | Some m -> m
-            | None -> Map.empty
-        match initFlags, initRegs, initMem with
-        | f, Ok r, m ->
-            Ok {
-                Fl = f;
-                Regs = r;
-                MM = m;
-            }
-        | _, Error s, _ -> Error s
-
-    
-    // map a function which returns its own error type
-    // to one which returns a top-level error type
-    let mapErr fMap ins = 
-        match ins with
-        | Error s -> Error (fMap s)
-        | Ok x -> Ok x
-
-    // function to execute any parsed instruction from any module
-    // returns the modified datapath (or an error)
-    let execParsedLine ins d (symtab: SymbolTable) =
-        let exec' p f ins' err = 
-            { PInstr = ins'; PLabel = p.PLabel; PCond = p.PCond; PSize = p.PSize }
-            |> f
-            |> mapErr err
-
-        ins |> Result.bind (
-            function
-            | {PInstr=IMULTMEM ins'} as p -> 
-                exec' p (MultMem.execInstr d) ins' ERRIMULTMEM
-                |> Result.bind (fun cpu -> Ok (cpu, symtab))
-            | {PInstr=IARITH ins'} as p -> 
-                exec' p (Arithmetic.execArithmeticInstr d) ins' ERRIARITH
-                |> Result.bind (fun cpu -> Ok (cpu, symtab))
-        )
-
-    /// takes a list of lines as string
-    /// and optionally, a symbol table
-    /// parses each line using parseLine function
-    /// returns a list of Ok parsed instructions, or errors
-    let parseLines lines symtab = 
-        let rec parseLines' lines loadaddr = 
-            match lines with
-            // no more lines to parse, return empty
-            | [] -> [] 
-            | line :: rest ->
-                parseLine symtab (WA loadaddr) line
-                |> function
-                    | Ok p -> Ok p :: parseLines' rest (loadaddr + p.PSize)
-                    // not sure how to increment size if error
-                    | Error s -> Error s :: parseLines' rest (loadaddr + 4u)
-        parseLines' lines 0u
-
-
-    /// execute a list of parsed lines
-    /// returns modified DataPath and symbol table
-    /// binds any errors in the execution or parsed lines
-    let execParsedLines parsedLines cpuData symtab = 
-        let rec execLines lines cpu' symtab' = 
-            match lines with
-            // no more lines to return, so return cpu and symtab
-            | [] -> Ok (cpu', symtab')
-            | line :: rest ->
-                execParsedLine line cpu' symtab'
-                |> Result.bind (fun (newCpu, newSymTab) -> execLines rest newCpu newSymTab)
-        execLines parsedLines cpuData symtab
